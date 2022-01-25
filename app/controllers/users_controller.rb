2eae9449049244f316044e27a9ebeb12b985a1fd
PapicoRoom <k.y_y.m@icloud.com>
2022-01-25 22:11:40 +0900

    users_controller に show メソッドを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
   def index
+    binding.pry
     @users = User.all
+  end
 
-    render json: @users
+  def show
+    @user = User.find(params[:id])
   end
 end
