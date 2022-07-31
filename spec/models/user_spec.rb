d6ad4710580d9288bec4544e3d247a11eb457202
PapicoRoom <k.y_y.m@icloud.com>
2022-07-31 17:17:02 +0900

    rails_helper.rb に config.include FactoryBot::Syntax::Methods の記載追記したので、FactoryBotを省略化できる

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   context "account を指定しているとき" do
     it "ユーザが作られる" do
-      user = User.new(name: "foo", account: "foo", email: "foo@example.com")
+      user = FactoryBot.build(:user)
+      # user = User.new(name: "foo", account: "foo", email: "foo@example.com")
       # expect(user.valid?).to eq true
       expect(user).to be_valid
     end
@@ -11,7 +12,8 @@ RSpec.describe User, type: :model do
 
   context "account を指定していないとき" do
     it "ユーザ作成に失敗する" do
-      user = User.new(name: "foo", email: "foo@example.com")
+      user = FactoryBot.build(:user, account: nil)
+      # user = User.new(name: "foo", email: "foo@example.com")
       expect(user).to be_invalid
       expect(user.errors.details[:account][0][:error]).to eq :blank
     end
@@ -26,8 +28,10 @@ RSpec.describe User, type: :model do
 
   context "すでに同じ名前の account が存在しているとき" do
     it "ユーザ作成に失敗する" do
-      User.create!(name: "foo", account: "foo", email: "foo.example.com")
-      user = User.new(name: "bar", account: "foo", email: "foo.example.com")
+      FactoryBot.create(:user, account: "kaitaro")
+      user = FactoryBot.build(:user, account: "kaitaro")
+      # User.create!(name: "foo", account: "foo", email: "foo.example.com")
+      # user = User.new(name: "bar", account: "foo", email: "foo.example.com")
       expect(user).to be_invalid
       # binding.pry
       expect(user.errors.details[:account][0][:error]).to eq :taken
